95958d84433740e35bf52894bb12a42359510671
Igor Josafá <igor.josafa@conveniar.com.br>
2026-06-26 18:42:16 -0300

    Melhorando o design da tela de pendências

--- app/src/main/java/com/example/conveniar_coordenador/PedidosActivity.java
+++ app/src/main/java/com/example/conveniar_coordenador/PedidosActivity.java
@@ -1,11 +1,7 @@
 package com.example.conveniar_coordenador;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,18 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.example.conveniar_coordenador.databinding.ActivityPedidosBinding;
 
 public class PedidosActivity extends BaseActivity {
+
+    private ActivityPedidosBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_pedidos);
+        binding = ActivityPedidosBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        // Ajuste para respeitar a área da Status Bar
+        ViewCompat.setOnApplyWindowInsetsListener(binding.mainContent, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -33,56 +32,62 @@ public class PedidosActivity extends BaseActivity {
         // Chama o método da BaseActivity que configura o Menu e o Nome
         setupDrawer();
 
-        configurarListaPedidos();
+        configurarCliques();
     }
 
-    private void configurarListaPedidos() {
-        ListView lista_opcoes = findViewById(R.id.list_opcoes_pedidos);
-        List<ItemMenu> lista = new ArrayList<>();
-
-        lista.add(new ItemMenu("Compra/Serviço", R.drawable.icone_compra));
-        lista.add(new ItemMenu("Adiantamento", R.drawable.icone_adiantamento));
-        lista.add(new ItemMenu("Acerto de Adiantamento", R.drawable.icone_acertoadiantamento));
-        lista.add(new ItemMenu("Reembolso", R.drawable.icone_reembolso));
-        lista.add(new ItemMenu("Pagamento de Diárias/Frete", R.drawable.icone_diaria));
-        lista.add(new ItemMenu("Pagamento de Bolsa", R.drawable.icone_pag_bolsa));
-        lista.add(new ItemMenu("Pagamento de Pessoa Jurídica", R.drawable.icone_pag_juridica));
-        lista.add(new ItemMenu("Pagamento de Pessoa Física", R.drawable.icone_pag_fisica));
-        lista.add(new ItemMenu("Entrada de Receita", R.drawable.icone_entradareceita));
-        lista.add(new ItemMenu("Transferência Entre Projetos", R.drawable.icone_transferencia));
-        lista.add(new ItemMenu("Parecer Técnico", R.drawable.icone_parecer));
-        lista.add(new ItemMenu("Pagamento de Bolsa em Lote", R.drawable.icone_bolsalote));
-        lista.add(new ItemMenu("Ordens de Pagamento de AF/OS", R.drawable.icone_ordem));
-        lista.add(new ItemMenu("Contratação de Pessoas", R.drawable.icone_contratacao));
-
-        MenuAdapter adapter = new MenuAdapter(this, lista);
-        lista_opcoes.setAdapter(adapter);
-
-        lista_opcoes.setOnItemClickListener((parent, view, position, id) -> Navegar_Portal(position));
+    private void configurarCliques() {
+        // Vincula cada Card ao seu respectivo link do portal abrindo na WebView interna
+        binding.cardCompra.setOnClickListener(v -> Navegar_Portal(0));
+        binding.cardAdiantamento.setOnClickListener(v -> Navegar_Portal(1));
+        
+        if (binding.cardAcertoAdiantamento != null) {
+            binding.cardAcertoAdiantamento.setOnClickListener(v -> Navegar_Portal(2));
+        }
+        
+        binding.cardReembolso.setOnClickListener(v -> Navegar_Portal(3));
+        binding.cardDiaria.setOnClickListener(v -> Navegar_Portal(4));
+        
+        if (binding.cardBolsa != null) {
+            binding.cardBolsa.setOnClickListener(v -> Navegar_Portal(5));
+        }
+        
+        binding.cardPagPf.setOnClickListener(v -> Navegar_Portal(7));
+        binding.cardPagPj.setOnClickListener(v -> Navegar_Portal(6));
+        
+        if (binding.cardReceita != null) binding.cardReceita.setOnClickListener(v -> Navegar_Portal(8));
+        if (binding.cardTransferencia != null) binding.cardTransferencia.setOnClickListener(v -> Navegar_Portal(9));
+        if (binding.cardParecer != null) binding.cardParecer.setOnClickListener(v -> Navegar_Portal(10));
+        if (binding.cardBolsaLote != null) binding.cardBolsaLote.setOnClickListener(v -> Navegar_Portal(11));
+        if (binding.cardOrdem != null) binding.cardOrdem.setOnClickListener(v -> Navegar_Portal(12));
+        if (binding.cardContratacao != null) binding.cardContratacao.setOnClickListener(v -> Navegar_Portal(13));
     }
 
     public void Navegar_Portal(int posicao) {
-        Uri uri = null;
+        String url;
         switch (posicao) {
-            case 0: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PedidoCompra.aspx"); break;
-            case 1: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoAdiantamento.aspx"); break;
-            case 2: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PedidoAcertoAdiantamento.aspx"); break;
-            case 3: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoReembolso.aspx"); break;
-            case 4: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoDiaria.aspx"); break;
-            case 5: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoBolsa.aspx"); break;
-            case 6: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoPessoaJuridica.aspx"); break;
-            case 7: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoPessoaFisica.aspx"); break;
-            case 8: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/EntradaDeReceita.aspx"); break;
-            case 9: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoTransferencia.aspx"); break;
-            case 10: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PedidoParecerTecnico.aspx"); break;
-            case 11: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoBolsaLote.aspx"); break;
-            case 12: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/OPCompraAF.aspx"); break;
-            case 13: uri = Uri.parse("https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PedidoContratacao.aspx"); break;
+            case 0: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PedidoCompra.aspx"; break;
+            case 1: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoAdiantamento.aspx"; break;
+            case 2: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PedidoAcertoAdiantamento.aspx"; break;
+            case 3: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoReembolso.aspx"; break;
+            case 4: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoDiaria.aspx"; break;
+            case 5: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoBolsa.aspx"; break;
+            case 6: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoPessoaJuridica.aspx"; break;
+            case 7: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoPessoaFisica.aspx"; break;
+            case 8: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/EntradaDeReceita.aspx"; break;
+            case 9: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoTransferencia.aspx"; break;
+            case 10: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PedidoParecerTecnico.aspx"; break;
+            case 11: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PagamentoBolsaLote.aspx"; break;
+            case 12: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/OPCompraAF.aspx"; break;
+            case 13: url = "https://cientec.conveniar.com.br/Coordenador/Forms/Pesquisador/PedidoContratacao.aspx"; break;
             default:
                 Toast.makeText(this, "Opção inválida", Toast.LENGTH_SHORT).show();
                 return;
         }
-        Intent it_site = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(it_site);
+
+        // Abrir na WebViewActivity interna do aplicativo passando o TOKEN
+        Intent intent = new Intent(this, WebViewActivity.class);
+        intent.putExtra("URL", url);
+        intent.putExtra("TOKEN", token);
+        startActivity(intent);
     }
 }
